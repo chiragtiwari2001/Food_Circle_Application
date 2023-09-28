204ba4b1ef5b944280650ef48b87d9c6a5b07bb0
chiragtiwari2001 <chiragtiwari2001@gmail.com>
2023-09-28 18:56:01 +0530

    Added Mail Confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   after_create :assign_default_role
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable 
+         :recoverable, :rememberable, :validatable, :confirmable 
   
   validates :username, presence: true, length: {maximum: 15}
   validates :email, length: {maximum: 255}, format: {with: email_regexp}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_28_055757) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_28_131110) do
   create_table "restaurants", force: :cascade do |t|
     t.string "restaurant_name"
     t.string "restaurant_email"
@@ -43,6 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_28_055757) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.text "address"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
